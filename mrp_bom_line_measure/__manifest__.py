67176282c8dce4965bd1a8da282cdcf287e143ad
José L. S. A <alagunasalahaddin@live.com>
2018-10-26 14:45:35 +0300

    [mrp_bom_line_measure] :sunny: include init and manifest file.

---
+++ mrp_bom_line_measure/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'MRP Bom Line Measures',
+    'version': '11.0',
+    'author': 'José Luis Sandoval Alaguna',
+    'website': '',
+    'category': 'Manufacture',
+    'sequence': 15,
+    'summary': '',
+    'images': [],
+    'depends': [
+        'purchase',
+        'mrp',
+        'sale_stock',
+        'stock'
+    ],
+    'description': """
+Manage measures for items in Product's BoM.
+==============================================
+    """,
+    'data': [
+        'views/mrp_bom_lines.xml',
+        'views/purchase_views.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
