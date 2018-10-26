7f816533338ad2e2ea46984b1b0e0a39e6d1f84f
José L. S. A <alagunasalahaddin@live.com>
2018-10-26 09:46:46 +0300

    [mrp_bom_line_measure] :fire: Append fields to procurement.rule._prepare_purchase_order_line()

--- mrp_bom_line_measure/models/purchase.py
+++ mrp_bom_line_measure/models/purchase.py
@@ -19,3 +19,17 @@ class PurchaseOrderLine(models.Model):
         'Unit of Measure',
         help="Unit of Measure (Unit of Measure) is the unit of measurement for the products measure"
     )
+
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    @api.multi
+    def _prepare_purchase_order_line(self, product_id, measure, measure_uom_id, product_qty, product_uom, values, po,
+                                     supplier):
+        result = super(PurchaseOrderLine, self)._prepare_purchase_order_line(product_id, product_qty, product_uom,
+                                                                             values, po, supplier)
+        result['measure'] = measure
+        result['measure_uom_id'] = measure_uom_id
+
+        return result
