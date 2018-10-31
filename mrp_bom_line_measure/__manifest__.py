59a95d1d54eb50f74addde932461f748b3ac855c
José L. S. A <alagunasalahaddin@live.com>
2018-10-31 17:15:08 +0300

    [mrp_bom_line_measure] :lollipop: added custom fields to purchase reports.

--- mrp_bom_line_measure/__manifest__.py
+++ mrp_bom_line_measure/__manifest__.py
@@ -20,7 +20,9 @@ Manage measures for items in Product's BoM.
     'data': [
         'views/mrp_bom_lines.xml',
         'views/purchase_views.xml',
-        'report/mrp_bom_structure_report_templates.xml'
+        'report/mrp_bom_structure_report_templates.xml',
+        'report/purchase_order_templates.xml',
+        'report/purchase_quotation_templates.xml'
     ],
     'demo': [],
     'test': [],
