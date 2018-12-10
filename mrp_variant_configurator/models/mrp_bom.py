991cda0c4671146fb8f90d68eca45c3bd14caa10
José L. S. A <alagunasalahaddin@live.com>
2018-12-10 12:47:13 +0300

    [mrp_variant_configurator] :fire: remove unused code.

--- mrp_variant_configurator/models/mrp_bom.py
+++ mrp_variant_configurator/models/mrp_bom.py
@@ -3,27 +3,6 @@
 from odoo import models, api
 
 
-class MrpBom(models.Model):
-    _inherit = 'mrp.bom'
-
-    @api.multi
-    def write(self):
-        product_obj = self.env['product.product']
-        lines = self.mapped('order_line').filtered(lambda x: not x.product_id)
-        for line in lines:
-            product = product_obj._product_find(
-                line.product_tmpl_id, line.product_attribute_ids,
-            )
-            if not product:
-                values = line.product_attribute_ids.mapped('value_id')
-                product = product_obj.create({
-                    'product_tmpl_id': line.product_tmpl_id.id,
-                    'attribute_value_ids': [(6, 0, values.ids)],
-                })
-            line.write({'product_id': product.id})
-        super(MrpBom, self).action_confirm()
-
-
 class MrpBomLine(models.Model):
     _inherit = ['mrp.bom.line', 'product.configurator']
     _name = 'mrp.bom.line'
